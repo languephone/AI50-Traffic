2dc6ff7ef46d642ddab8144a87306c347c453283
languephone <73309519+languephone@users.noreply.github.com>
2022-02-13 16:16:14 +0000

    First attempt of get_model function.

--- traffic.py
+++ traffic.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import os
 import sys
-# import tensorflow as tf
+import tensorflow as tf
 
 from sklearn.model_selection import train_test_split
 
@@ -79,8 +79,35 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+    model = tf.keras.models.Sequential()
 
+    # Convolutional layer. Learn 32 filters using a 3x3 kernel
+    tf.keras.layers.Conv2D(
+        32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+    ),
+
+    # Max-pooling layer, using 2x2 pool size
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+    # Flatten units
+    tf.keras.layers.Flatten(),
+
+    # Add a hidden layer with dropout
+    tf.keras.layers.Dense(128, activation="relu"),
+    tf.keras.layers.Dropout(0.5),
+
+    # Add an output layer with output units for all 10 digits
+    tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+
+
+    # Train neural network
+    model.compile(
+    optimizer="adam",
+    loss="binary_crossentropy",
+    metrics=["accuracy"]
+    )
+
+    return model
 
 if __name__ == "__main__":
     main()
