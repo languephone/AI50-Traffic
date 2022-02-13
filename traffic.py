b56ae8a3697a20a2f2bee5941fe589f5e65d9b54
languephone <73309519+languephone@users.noreply.github.com>
2022-02-13 16:29:17 +0000

    Working initial get_model function.

--- traffic.py
+++ traffic.py
@@ -67,7 +67,6 @@ def load_data(data_dir):
         for image in os.listdir(path):
             img = cv2.imread(os.path.join(data_dir, str(i), image))
             img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
-            img = np.array(img)
             images.append(img)
             labels.append(i)
 
@@ -79,26 +78,26 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    model = tf.keras.models.Sequential()
+    model = tf.keras.models.Sequential([
 
-    # Convolutional layer. Learn 32 filters using a 3x3 kernel
-    tf.keras.layers.Conv2D(
-        32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
-    ),
+        # Convolutional layer. Learn 32 filters using a 3x3 kernel
+        tf.keras.layers.Conv2D(
+            32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        ),
 
-    # Max-pooling layer, using 2x2 pool size
-    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        # Max-pooling layer, using 2x2 pool size
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
 
-    # Flatten units
-    tf.keras.layers.Flatten(),
+        # Flatten units
+        tf.keras.layers.Flatten(),
 
-    # Add a hidden layer with dropout
-    tf.keras.layers.Dense(128, activation="relu"),
-    tf.keras.layers.Dropout(0.5),
+        # Add a hidden layer with dropout
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dropout(0.5),
 
-    # Add an output layer with output units for all 10 digits
-    tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
-
+        # Add an output layer with output units for all 10 digits
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+    ])
 
     # Train neural network
     model.compile(
